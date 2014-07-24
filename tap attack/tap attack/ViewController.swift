551dea7e81294c90558af2bed6ca674e022496a0
Michael Hamel <hamel.michael.p@gmail.com>
2014-07-23 19:36:18 -0500

    Added project skeleton.

---
+++ tap attack/tap attack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tap attack
+//
+//  Created by Michael Hamel on 7/23/14.
+//  Copyright (c) 2014 Michael Hamel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
