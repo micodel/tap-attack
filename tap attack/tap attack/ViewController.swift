237ec1b2320242833812b15f6a5d42f1db6fee84
Michael Hamel <hamel.michael.p@gmail.com>
2014-07-25 18:10:32 -0500

    Initialized varaibles for current time and score. Added method to increase score by 1 per button click.

--- tap attack/tap attack/ViewController.swift
+++ tap attack/tap attack/ViewController.swift
@@ -10,15 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Variables connected to labels and buttons on main view.
     @IBOutlet var timeLabel:UILabel
     @IBOutlet var scoreLabel:UILabel
     @IBOutlet var tapButton:UIButton
+    
+    // Variables to contain score and time per round.
+    var currentScore = Int()
+    var currentTime = Int()
+    
+    
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    // Function to increase score by 1 (per button click).
+    @IBAction func updateScore() {
+        ++currentScore
+        // Refreshes scoreLabel text to reflect new score.
+        scoreLabel.text = "Score: \(currentScore)"
+    }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
