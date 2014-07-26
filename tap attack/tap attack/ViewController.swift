05b82d90bb7eb5f27c25bf8f68962f1694423dd0
Michael Hamel <hamel.michael.p@gmail.com>
2014-07-26 14:46:38 -0500

    Improved OOD for renderGameOverView function.

--- tap attack/tap attack/ViewController.swift
+++ tap attack/tap attack/ViewController.swift
@@ -84,37 +84,36 @@ class ViewController: UIViewController {
         }
     }
     
-    // Defines game over view and appends over main view.
-    func renderGameOverView() {
+    
+    // Defines size and bg color of gameover view.
+    func defineGameOverView() {
         gameOverView = UIView(frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height))
         
         gameOverView.backgroundColor = UIColor.blackColor()
-        
-        // Lays game over view on top of main view.
-        self.view.addSubview(gameOverView)
-        
+    }
+    
+    // Defines and appends title image to gameover view.
+    func appendGameOverImage() {
         var gameOverImage = UIImageView(frame: CGRectMake(40, 93, 240, 128))
         // Renders image found in images.xcassets by name.
         gameOverImage.image = UIImage(named: "game-over")
-        gameOverView.addSubview(gameOverImage)
         
-        // Defines and adds label to display final score.
+        gameOverView.addSubview(gameOverImage)
+    }
+    
+    // Defines and adds label to display final score.
+    func appendFinalScoreLabel() {
+        
         var finalScoreLabel = UILabel(frame: CGRectMake(0, 267, 320, 20))
         finalScoreLabel.text = "Final Score \(currentScore)"
         finalScoreLabel.textColor = UIColor.whiteColor()
         finalScoreLabel.textAlignment = NSTextAlignment.Center
         
         gameOverView.addSubview(finalScoreLabel)
-        
-        // Defines and adds play again button to game over view.
-        var playAgainButton = UIButton(frame: CGRectMake(0, 396, gameOverView.frame.size.width, 55))
-        playAgainButton.setTitle("Play Again", forState: UIControlState.Normal)
-        playAgainButton.backgroundColor = UIColor.redColor()
-        playAgainButton.addTarget(self, action: Selector("startGame"), forControlEvents: UIControlEvents.TouchUpInside)
-        gameOverView.addSubview(playAgainButton)
-        
-        
-        //  HIGHSCORE
+    }
+    
+    // Defines and adds highscore label to gameover view.
+    func appendHighScoreLabel() {
         var defaults = NSUserDefaults()
         var localHighScore = defaults.integerForKey("highScore")
         if currentScore > localHighScore {
@@ -128,7 +127,34 @@ class ViewController: UIViewController {
         highScoreLabel.textAlignment = NSTextAlignment.Center
         
         gameOverView.addSubview(highScoreLabel)
-
+    }
+    
+    // Defines and adds play again button to game over view.
+    func appendPlayAgainButton() {
+    
+        var playAgainButton = UIButton(frame: CGRectMake(0, 396, gameOverView.frame.size.width, 55))
+        playAgainButton.setTitle("Play Again", forState: UIControlState.Normal)
+        playAgainButton.backgroundColor = UIColor.redColor()
+        playAgainButton.addTarget(self, action: Selector("startGame"), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        gameOverView.addSubview(playAgainButton)
+    }
+    
+    
+    // Defines game over view and appends over main view.
+    func renderGameOverView() {
+        
+        defineGameOverView()
+        
+        // Lays game over view on top of main view.
+        self.view.addSubview(gameOverView)
+        
+        appendGameOverImage()
+        appendFinalScoreLabel()
+        appendHighScoreLabel()
+        
+        appendPlayAgainButton()
+        
     }
 
 
